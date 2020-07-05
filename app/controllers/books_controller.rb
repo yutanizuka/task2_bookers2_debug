b8fbac9a1f271b70b9ca47b8ce477f935ccd7ed7
nizutaro <niduka81@gmail.com>
2020-07-05 16:55:33 +0900

    initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+	before_action :authenticate_user!
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def index
+    # @book = Book.new(book_params)
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    if @book.user != current_user
+      # redirect_to book_path(@book)
+       redirect_to books_path
+    end
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
+
